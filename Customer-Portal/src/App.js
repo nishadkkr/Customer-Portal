ea058355ce942a2e9d6f5cfad6a6d71f9df3b92f
p-nishad <nishadntechit@gmail.com>
2023-06-16 14:45:45 +0530

    Change app

--- Customer-Portal/src/App.js
+++ Customer-Portal/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Customer Portal.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://example.com"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more.
+          Learn React
         </a>
       </header>
     </div>
--- Customer-Portal/src/App.test.js
+++ Customer-Portal/src/App.test.js
@@ -1,14 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders welcome message', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const textElement = screen.getByText(/Customer Portal/i);
-  expect(textElement).toBeInTheDocument();
-});
-
-test('renders app link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn more/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
